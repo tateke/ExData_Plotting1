60ce9b30f31b28c1b5ba7b117866877dc0ea6848
Keith Tate <tateke@yahoo.com>
2015-07-10 06:44:05 -0700

    Fix formatting on plot3

--- plot3.R
+++ plot3.R
@@ -21,12 +21,14 @@ epc$Datetime <- as.POSIXct(datetime)
 par(mfrow=c(1,1),mar=c(4,4,2,1))
 
 ## Plot
-with(epc, {
-    plot(Sub_metering_1~Datetime, type="l", ylab="Energy sub metering", xlab="")
-    lines(Sub_metering_2~Datetime,col='Red')
-    lines(Sub_metering_3~Datetime,col='Blue')
-    legend("topright", col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=1,)
-})
+with(epc, 
+    {
+        plot(Sub_metering_1~Datetime, type="l", ylab="Energy sub metering", xlab="")
+        lines(Sub_metering_2~Datetime,col='Red')
+        lines(Sub_metering_3~Datetime,col='Blue')
+        legend("topright", col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=1,)
+    }
+)
 
 dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
